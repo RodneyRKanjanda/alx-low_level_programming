528d887e888af4ed988940806e61943914d02c3e
RodneyRKanjanda <rodneykanjanda@gmail.com>
2022-09-13 10:43:35 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - alphabet
+ *
+ * Return: nothing 
+ */
+void print_alphabet(void);
+{
+	char = i;
+
+	for(i = 'a';a <= 'z'; i++);
+		_putchar(i);
+	_putchar('\n');
+}
