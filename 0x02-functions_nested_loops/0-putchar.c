d667226b0109c867f94686e33e140c4142482e63
RodneyRKanjanda <rodneykanjanda@gmail.com>
2022-09-13 09:44:30 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * main - Print _putchar using the _putchar function
+ *
+ * Return: 0 (if succesful)
+ */
+
+int main(void)
+{
+	char word[] = "_putchar";
+
+	_putchar(word);
+	_putchar('\n');
+
+	return (0);
+}
+
+
