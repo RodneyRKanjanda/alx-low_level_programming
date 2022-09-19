cf98735c5e49016437e389265f3c285980585616
RodneyRKanjanda <rodneykanjanda@gmail.com>
2022-09-19 11:19:52 -0700

    Mandatory done

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,11 @@
+int _putchar(char);
+void reset_to_98(int);
+int _strlen(char);
+void swap_int(int, int);
+void _puts(char);
+void print_rev(char);
+void rev_string(char);
+void puts2(char);
+void puts_half(char);
+void print_array(int, int);
+char *_strcpy(char, char);
