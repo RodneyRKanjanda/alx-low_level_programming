f079397c1993f74a6885a0484a11070e9d78ac5e
RodneyRKanjanda <rodneykanjanda@gmail.com>
2022-09-28 12:47:29 -0700

    0x08-recursion[

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - function that prints a string, followed by new line
+ * @s: an input string to print
+ * Return: Nothing
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+		_putchar('\n');
+}
+
