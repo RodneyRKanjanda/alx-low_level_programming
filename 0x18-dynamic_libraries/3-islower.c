c1effeac78cce257331954426c6b5d58ca0a75e8
RodneyRKanjanda <rodneykanjanda@gmail.com>
2022-12-14 03:35:34 -0800

    libdynamic.so

---
+++ 0x18-dynamic_libraries/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _islower - checks for lower case characters
+ * @c: an input character
+ *
+ * Return: 1 if c is lowercase, 0 otherwise
+ */
+int _islower(int c)
+{
+	char i;
+	int j = 0;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i == c)
+			j = 1;
+	}
+	return (j);
+}
