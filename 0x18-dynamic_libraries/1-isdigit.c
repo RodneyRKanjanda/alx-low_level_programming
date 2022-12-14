50ff8e3f2012101896e24b37f90f0f64ae7e1098
RodneyRKanjanda <rodneykanjanda@gmail.com>
2022-12-14 03:15:48 -0800

    Mandatory done

---
+++ 0x18-dynamic_libraries/1-isdigit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _isdigit - checks if input character is a number
+ * @c: input character
+ * Return: 1 if it is a digit or 0 otherwise
+ */
+
+int _isdigit(int c)
+{
+	int j;
+	int isdigit = 0;
+
+	for (j = '0'; j <= '9'; j++)
+	{
+		if (c == j)
+		{
+			isdigit = 1;
+			break;
+		}
+	}
+	return (isdigit);
+}
+
---
+++ 0x18-dynamic_libraries/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - check the absolute value of an integer
+ * @r: integer input
+ *
+ * Return: absolute value of r
+ */
+int _abs(int r)
+{
+	if (r >= 0)
+		return (r);
+	else
+		return (r * -1);
+}
